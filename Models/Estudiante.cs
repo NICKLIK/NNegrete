bf23cbe63ca405a489d450f294c47c67e6fc0a9e
NICKLIK <niko.negrete.v@gmail.com>
2024-05-02 07:35:37 -0500

    creacion de la clase Estudiante

---
+++ Models/Estudiante.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NNegrete.Models
+{
+    public class Estudiante
+    {
+        [Key]
+        public int IdBanner { get; set; }
+        public float Promedio { get; set; }
+        [Required] 
+        public String Nombre { get; set; }
+        public Boolean Matriculado { get; set; }
+         
+
+    }
+}
